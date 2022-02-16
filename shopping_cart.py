b4987d6b7b6b8a90f8ede30b5dce681ca129815e
frankjeung <97686278+frankjeung@users.noreply.github.com>
2022-02-15 21:26:28 -0500

    Practice inputs and lookups

--- shopping_cart.py
+++ shopping_cart.py
@@ -37,6 +37,35 @@ def to_usd(my_price):
     return f"${my_price:,.2f}" #> $12,000.71
 
 
-# TODO: write some Python code here to produce the desired output
 
-print(products)
+
+# ASK FOR USER INPUT
+
+product_id = input("Please input a product identifier: ")
+print(product_id) #> "9"
+print(type(product_id)) #> string
+
+
+# LOOK UP CORRESPONDING PRODUCTS
+
+# print product that has an ide attribute equal to "9"
+
+matching_products = []
+
+for x in products:
+    #if x== 3:
+    #    ___.append(X)
+    #print(x)
+    #print(x["id"])
+    if str(x["id"]) == str(product_id):
+        # this is a match
+        matching_products.append(x)
+
+print(matching_products)
+print(type(matching_products))
+print(len(matching_products))
+# print the name of the matching product
+matching_product =matching_products[0]
+print(matching_product["name"], matching_product["price"])
+
+
