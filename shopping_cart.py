3b554d7ba1f4502264d1d9487e880e0aaa2c5a21
frankjeung <97686278+frankjeung@users.noreply.github.com>
2022-02-21 09:41:23 -0500

    Assemble running total price

--- shopping_cart.py
+++ shopping_cart.py
@@ -23,35 +23,6 @@ products = [
     {"id":20, "name": "Pomegranate Cranberry & Aloe Vera Enrich Drink", "department": "beverages", "aisle": "juice nectars", "price": 4.25}
 ] # based on data from Instacart: https://www.instacart.com/datasets/grocery-shopping-2017
 
-#
-# INFO CAPTURE / INPUT
-#
-
-while True:
-    selected_id = input("Please input a product identifier") #> "9" (string)
-    #> "DONE"
-    if selected_id == "DONE":
-        break
-    else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"])
-
-#
-# INFO DISPLAY / OTUPUT
-#
-
-
-
-# A grocery store name of your choice
-# A grocery store phone number and/or website URL and/or address of choice
-# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
-# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
-# The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
-# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
-# The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
-# A friendly message thanking the customer and/or encouraging the customer to shop again
-
 def to_usd(my_price):
     """
     Converts a numeric value to usd-formatted string, for printing and display purposes.
@@ -64,7 +35,37 @@ def to_usd(my_price):
     """
     return f"${my_price:,.2f}" #> $12,000.71
 
+#
+# INFO CAPTURE / INPUT
+#
 
+total_price = 0
+
+while True:
+    selected_id = input("Please input a product identifier") #> "9" (string)
+    if selected_id == "DONE":
+        break
+    else:
+        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        matching_product = matching_products[0]
+        total_price = total_price + matching_product["price"]
+        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"])
+
+#
+# INFO DISPLAY / OTUPUT
+#
+
+
+
+print("TOTAL PRICE: " + str(total_price))
+# A grocery store name of your choice
+# A grocery store phone number and/or website URL and/or address of choice
+# The date and time of the beginning of the checkout process, formatted in a human-friendly way (e.g. 2020-02-07 03:54 PM)
+# The name and price of each shopping cart item, price being formatted as US dollars and cents (e.g. $3.50, etc.)
+# The total cost of all shopping cart items (i.e. the "subtotal"), formatted as US dollars and cents (e.g. $19.47), calculated as the sum of their prices
+# The amount of tax owed (e.g. $1.70), calculated by multiplying the total cost by a New York City sales tax rate of 8.75% (for the purposes of this project, groceries are not exempt from sales tax)
+# The total amount owed, formatted as US dollars and cents (e.g. $21.17), calculated by adding together the amount of tax owed plus the total cost of all shopping cart items
+# A friendly message thanking the customer and/or encouraging the customer to shop again
 
 while True:
     # ASK FOR USER INPUT
